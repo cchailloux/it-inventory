738bde57524b48a98d8d32d950cf5b7a5f4613a4
cchailloux <66261478+cchailloux@users.noreply.github.com>
2025-01-26 20:20:31 +0100

    test(electricity): add test and comment for generateYearMonthPairs

--- src/electricity.ts
+++ src/electricity.ts
@@ -1,12 +1,14 @@
 import { getAllDevices } from "./data/inventory";
 import { DeviceInventory } from "./data/types";
 
-const generateYearMonthPairs = (startDate: Date, endDate: Date): string[] => {
+export const generateYearMonthPairs = (startDate: Date, endDate: Date): string[] => {
   const yearMonthPairs: string[] = [];
   let currentDate = new Date(startDate);
+  
   // early return to avoir error case - better option would be to handle it
   if (currentDate > endDate) return yearMonthPairs;
 
+  //  TO IMPROVE - for now, we generate also the endDate month - even if it's not over
   while (currentDate <= endDate) {
     const year = currentDate.getFullYear();
     const month = currentDate.getMonth() + 1; // months are zero-based which can be counter-intuitive
@@ -22,15 +24,22 @@ const generateYearMonthPairs = (startDate: Date, endDate: Date): string[] => {
 
 /* 
  * Sums monthly electricity consumption for all devices
- * for all months where it's relevant - aka between now and oldest purchase
+ * for all months where it's relevant - aka between the limitDate and the oldest purchase
+ * TO IMPROVE - add possibility to specifiy a startDate
  */
-export const electricityRecapByMonth = (allDevices: DeviceInventory[], mustRound?: boolean): { [month: string]: number } => {
-    const todayDate = new Date();
+export const electricityRecapByMonth = (
+    allDevices: DeviceInventory[],
+    mustRound?: boolean,
+    limitDate?: Date,
+  ): { [month: string]: number } => {
+    
+    const endDate = limitDate ?? new Date();
     const monthElecDic: {[month: string]: number} = {}
 
     allDevices.forEach((device) => {
       const monthlyConsumption = device.annualElectricalConsumption / 12;
-      const yearMonthPairs = generateYearMonthPairs(device.dateOfPurchase, todayDate);
+      // TO IMPROVE - for now, the startDate is the month of purchase - even if that month was partially started
+      const yearMonthPairs = generateYearMonthPairs(device.dateOfPurchase, endDate);
 
       yearMonthPairs.forEach((yearMonthPair: string) => {
         monthElecDic[yearMonthPair]
--- tests/electricity.test.ts
+++ tests/electricity.test.ts
@@ -1,36 +1,63 @@
 import { DeviceInventory } from '../src/data/types';
-import { electricityRecapByMonth } from '../src/electricity';
+import { electricityRecapByMonth, generateYearMonthPairs } from '../src/electricity';
 
 const devices: DeviceInventory[] = [
     {
         computerName: 'Computer1',
         dateOfPurchase: new Date('2024-12-01'),
         priceOfPurchase: 1000,
-        annualElectricalConsumption: 1203
+        annualElectricalConsumption: 1800
      },
     {
         computerName: 'Computer2',
         dateOfPurchase: new Date('2024-11-15'),
         priceOfPurchase: 1500,
-        annualElectricalConsumption: 1800
+        annualElectricalConsumption: 1203
     }
 ];
 
+const startDate = new Date('2024-10-15');
+const limitDate = new Date('2024-12-31'); 
+
+describe('generateYearMonthPairs', () => {
+    it('should generate Year-Month pairs on the format YYYY-MM', () => {
+        const result = generateYearMonthPairs(startDate, limitDate);
+        expect(result).toEqual(["2024-10", "2024-11", "2024-12"]);
+    });
+});
+
 describe('electricityRecapByMonth', () => {
-    it('should calculate the rounded monthly consumption', () => {
-    const result = electricityRecapByMonth(devices, true);
-    expect(result).toEqual({
-      '2024-11': 150,
-      '2024-12': 250,
-      '2025-01': 250,
+    it('should calculate the rounded monthly consumption until now', () => {
+        const result = electricityRecapByMonth(devices, true);
+        expect(result).toEqual({
+            '2024-11': 100,
+            '2024-12': 250,
+            '2025-01': 250,
+        });
     });
+
+    it('should calculate the exact monthly consumption until now', () => {
+        const result = electricityRecapByMonth(devices);
+        expect(result).toEqual({
+            '2024-11': 100.25,
+            '2024-12': 250.25,
+            '2025-01': 250.25,
+        });
     });
-    it('should calculate the exact monthly consumption', () => {
-    const result = electricityRecapByMonth(devices);
-    expect(result).toEqual({
-      '2024-11': 150,
-      '2024-12': 250.25,
-      '2025-01': 250.25,
+
+    it('should calculate the rounded monthly consumption until specified date', () => {
+        const result = electricityRecapByMonth(devices, true, limitDate);
+        expect(result).toEqual({
+            '2024-11': 100,
+            '2024-12': 250,
+        });
     });
-  });
+
+    it('should calculate the exact monthly consumption until specified date', () => {
+        const result = electricityRecapByMonth(devices, undefined, limitDate);
+        expect(result).toEqual({
+            '2024-11': 100.25,
+            '2024-12': 250.25,
+        });
+    });
 });
